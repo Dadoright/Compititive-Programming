4770a6878b00e2a57fbd9792567caa413bd09861
Dadoright <donabusi1@gmail.com>
2021-01-14 20:12:26 +0530

    here is a good example why type casting is neccesary

---
+++ Watermelon.py
@@ -0,0 +1,15 @@
+weight = input()
+k = int(weight)%2
+
+if int(weight) == 2:
+    print("NO")
+
+
+else:
+    if k==0:
+        print("Yes")
+    else :
+        print("NO")
+
+
+#type casting should be done for input variables because without input variables casted problems occur
